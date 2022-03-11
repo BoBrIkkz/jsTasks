67a471f13784f29e131da0805e0530cbbdbafa40
BoBrIkkz <78279032+BoBrIkkz@users.noreply.github.com>
2022-03-11 11:31:33 +0300

    task 3

---
+++ task.js
@@ -0,0 +1,8 @@
+var words = ['Ночь', 'Улица', 'Фонарь', 'Аптека', 'Бессмысленный', 'Тусклый', 'Свет'];
+
+// возвращает Свет;
+
+function getLastElementOfArray(array) {
+    // Ваш код здесь
+    return array.pop()
+}
